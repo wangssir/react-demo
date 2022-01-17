3d3170c63d696d0a6cce06fb9506fecb2b8539f3
wangshiru <wangshiru@mchz.com.cn>
2022-01-17 16:20:26 +0800

    antd 样式隔离

--- src/pages/page12/index.jsx
+++ src/pages/page12/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './app'
 import './index.less';
 import { ConfigProvider } from 'antd';
-import 'antd/dist/antd.less';
+// import 'antd/dist/antd.less';
 
 ReactDOM.render(
     <App />
